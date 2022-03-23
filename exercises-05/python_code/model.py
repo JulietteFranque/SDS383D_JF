d59425d6c2229410db3451e3a1d4da5814482872
Juliette Franqueville <juliette.franqueville@gmail.com>
2022-03-23 16:29:36 -0500

    fix

--- exercises-05/python_code/model.py
+++ exercises-05/python_code/model.py
@@ -8,19 +8,19 @@ class Model:
         self.X_groups = X_groups
         self.y_groups = y_groups
         self.n_stores = len(self.X_groups)
-        self.P = 4
+        self.n_par = 4
         self.N = total_observations
-        self.tau_squared = 1
-        self.sigma_squared = 1
-        self.gamma = np.ones(self.P)
-        self.betas = np.ones((self.P, len(X_groups)))
+        self.tau_squared = 100
+        self.sigma_squared = 100
+        self.gamma = np.ones(self.n_par)
+        self.betas = np.ones((self.n_par, len(X_groups)))
         self.n_iter = n_iter
-        self.traces = {'betas': np.zeros([self.n_iter, self.P, len(X_groups)]), 'gamma': np.zeros((self.n_iter, self.P)), 'sigma_squared': np.zeros(self.n_iter), 'tau_squared': np.zeros(self.n_iter)}
+        self.traces = {'betas': np.zeros([self.n_iter, self.n_par, len(X_groups)]), 'gamma': np.zeros((self.n_iter, self.n_par)), 'sigma_squared': np.zeros(self.n_iter), 'tau_squared': np.zeros(self.n_iter)}
 
     def _update_betas(self):
         for store_number in range(self.n_stores):
-            K_inv = np.linalg.inv(1 / self.sigma_squared * self.X_groups[store_number].T @ self.X_groups[
-                store_number] + 1 / self.tau_squared * np.identity(self.P))
+            K_inv = np.linalg.inv(1 / self.sigma_squared * (self.X_groups[store_number].T @ self.X_groups[
+                store_number] + 1 / self.tau_squared * np.identity(self.n_par)))
             m = 1 / self.sigma_squared * K_inv @ (
                     self.X_groups[store_number].T @ self.y_groups[store_number] + 1 / self.tau_squared * self.gamma)
             self.betas[:, store_number] = multivariate_normal(m, K_inv).rvs()
@@ -34,13 +34,13 @@ class Model:
         self.sigma_squared = 1 / gamma(alpha, 1 / beta).rvs()
 
     def _update_gamma(self):
-        K_inv = np.linalg.inv(self.n_stores * np.identity(self.P) / (self.sigma_squared * self.tau_squared))
-        m = K_inv @ np.sum(self.betas, axis=1)
+        K_inv = np.linalg.inv(self.n_stores * np.identity(self.n_par) / (self.sigma_squared * self.tau_squared))
+        m = K_inv @ np.sum(self.betas, axis=1) / (self.sigma_squared * self.tau_squared)
         self.gamma = multivariate_normal(m, K_inv).rvs()
 
     def _update_tau_squared(self):
         alpha = self.n_stores + 1 / 2
-        beta = sum([ 0.5 * (
+        beta = sum([0.5 * (
                 (1 / self.sigma_squared * (self.betas[:, store_number] - self.gamma).T @ (self.betas[:, store_number] - self.gamma)) + 1) for
                     store_number in range(self.n_stores)])
         self.tau_squared = 1 / gamma(alpha, 1 / beta).rvs()
@@ -54,7 +54,7 @@ class Model:
     def _discard_burn(self):
         for key in self.traces.keys():
             self.traces[key] = self.traces[key][100:]
-            
+
     def fit(self):
         for it in tqdm(range(self.n_iter)):
             self._update_gamma()
